e830baa520da442fa969b1a8f78744814a7ace63
nikhilfufu <pallaviml007@outllook.com>
2024-04-07 13:36:46 +0530

    nikhil changes

--- javaScript.js
+++ javaScript.js
@@ -3,10 +3,16 @@ let person = {
     number:"1234",
     age:"20"
 }
-Object.defineProperty(person,{
-  number:{  writable:false},
-  age:{value:"20"}
+Object.defineProperty(person,"number",{
+  writable:false
 })
 person.number="54321"
 person.age="90"
 console.log(person)
+
+let s ="nikhil"
+for(let i = 0;i<s.length;i++){
+  let strings=s.split("")
+  console.log(strings.length)
+}
+s.to
